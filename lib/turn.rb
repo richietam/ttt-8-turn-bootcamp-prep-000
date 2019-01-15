de98548c12e80cbbed299bb94a5bbdb60b58bc82
richardtam.ny <richardtam.ny@gmail.com>
2019-01-15 22:04:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,4 +34,8 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
+  if (valid_move?(board,index)) == true
+    move(board, index, token)
+  else
+    puts "Please enter 1-9"
 end
